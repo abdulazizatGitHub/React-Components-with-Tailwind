3a523c08cc253f7e9a4b7ea672fe29b31ee4197b
Abdul Aziz <129079186+abdulazizatGitHub@users.noreply.github.com>
2024-04-26 08:20:28 +0500

    main

--- react-components-with-tailwind/src/App.js
+++ react-components-with-tailwind/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "bg-gray-200 p-4">
+      <h1 className = "text-2xl font-bold text-blue-600 font-sans">Hello, Welcome to Tailwind!</h1>
+      <button className = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Click Me</button>
     </div>
   );
 }
